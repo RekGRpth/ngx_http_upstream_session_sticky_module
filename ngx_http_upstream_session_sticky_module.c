c6f80649896a83cfac0b80a9ccf9b977439d3e77
RekGRpth <rekgrpth@gmail.com>
2022-02-14 16:12:49 +0500

    up

--- ngx_http_upstream_session_sticky_module.c
+++ ngx_http_upstream_session_sticky_module.c
@@ -1363,18 +1363,9 @@ ngx_http_upstream_session_sticky_init_upstream(ngx_conf_t *cf,
         sscf->server[i].check_index = peer->check_index;
 #endif
         if (sscf->flag & NGX_HTTP_SESSION_STICKY_PLAIN) {
-#if (T_NGX_HTTP_UPSTREAM_ID)
-            if (peer->id.len == 0) {
-#endif
-                sscf->server[i].sid.data = peer->name.data;
-                sscf->server[i].sid.len = peer->name.len;
-                continue;
-#if (T_NGX_HTTP_UPSTREAM_ID)
-            }
-
-            sscf->server[i].sid.data = peer->id.data;
-            sscf->server[i].sid.len = peer->id.len;
-#endif
+            sscf->server[i].sid.data = peer->name.data;
+            sscf->server[i].sid.len = peer->name.len;
+            continue;
 
         } else if (ngx_http_upstream_session_sticky_set_sid(
                                                 cf, &sscf->server[i]) != NGX_OK)
